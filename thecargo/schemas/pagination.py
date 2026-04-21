789f58b06fa8c8dabbb3bb9891b86da9f755d6f1
Baxtiyor Ergashev <b.ergashev@uic.group>
2026-04-21 16:54:43 +0500

    Restore PaginatedResponse for backward compatibility

--- thecargo/schemas/pagination.py
+++ thecargo/schemas/pagination.py
@@ -1,7 +1,18 @@
+from typing import Generic, TypeVar
+
 from fastapi_pagination import Page
 from fastapi_pagination.customization import CustomizedPage, UseParamsFields
+
+from thecargo.schemas.base import AppSchema
+
+T = TypeVar("T")
 
 CustomPage = CustomizedPage[
     Page,
     UseParamsFields(size=50),
 ]
+
+
+class PaginatedResponse(AppSchema, Generic[T]):
+    count: int
+    results: list[T]
